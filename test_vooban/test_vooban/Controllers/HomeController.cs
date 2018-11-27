9a8978263c320d9d8c153a9b5222e52d0e433c0a
Jean-Francois Bouchard <jeanfrancoisbouchard3@gmail.com>
2018-11-26 22:26:05 -0500

    Challenge 1 and 2 functional

--- test_vooban/test_vooban/Controllers/HomeController.cs
+++ test_vooban/test_vooban/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+
 
 namespace test_vooban.Controllers
 {
@@ -11,6 +13,105 @@ namespace test_vooban.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult countNamesWithAtLeastTwoVowels(HttpPostedFileBase fileInput)
+        {
+            Char[] vowels = { 'A', 'E', 'I', 'O', 'U', 'Y' };
+            int vowelCount = 0;
+            List<string> lstNames = new List<string>();
+            string nameEntry;
+
+            if (fileInput != null && fileInput.ContentLength > 0)
+            {
+                StreamReader sr = new StreamReader(fileInput.InputStream);
+                while (!sr.EndOfStream)
+                {
+                    nameEntry = sr.ReadLine();
+                    foreach (char c in nameEntry)
+                    {
+                        if (vowels.Contains(c))
+                        {
+                            vowelCount++;
+                        }
+                    }
+                    if (vowelCount >= 2)
+                    {
+                        lstNames.Add(nameEntry);
+                    }
+                    vowelCount = 0;
+                }
+                sr.Close();
+
+                if (lstNames.Count() > 0)
+                {
+                    string test = Server.MapPath("~");
+                    StreamWriter sw = new StreamWriter(Server.MapPath("~") + @"\sortedNames.txt");
+                    foreach (string line in lstNames)
+                    {
+                        sw.WriteLine(line);
+                    }
+                    sw.Close();
+                }
+                TempData["FirstMessage"] = String.Format("{0} noms ont été enregistrés.", lstNames.Count);
+            }
+            else
+            {
+                TempData["FirstMessage"] = "Aucun fichier selectionné.";
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult downloadFile()
+        {
+            string filePath = Server.MapPath("~") + @"\sortedNames.txt";
+            if (System.IO.File.Exists(filePath))
+            {
+                return GetDocument(filePath);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult calculateScore(HttpPostedFileBase fileInput)
+        {
+            bool negativeScoreReached = false;
+            char inputCharacter;
+            int negativeScoreIndex = 0, index = 0, score = 0;
+
+            if (fileInput != null && fileInput.ContentLength > 0)
+            {
+                StreamReader sr = new StreamReader(fileInput.InputStream);
+                while (!sr.EndOfStream)
+                {
+                    inputCharacter = (char)sr.Read();
+                    if (inputCharacter == '(')
+                    {
+                        score++;
+                    }
+                    else if (inputCharacter == ')')
+                    {
+                        score--;
+                    }
+
+                    if (!negativeScoreReached && score == -1)
+                    {
+                        negativeScoreReached = true;
+                        negativeScoreIndex = index;
+                    }
+                    index++;
+                }
+                TempData["SecondMessage"] = String.Format("Score final : {0}. Position du premier score négatif : {1}", score, negativeScoreIndex > 0 ? negativeScoreIndex.ToString() : "Aucun");
+            }
+            else
+            {
+                TempData["SecondMessage"] = "Aucun fichier selectionné.";
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
